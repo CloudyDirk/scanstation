a9240368e5a143ffe2788769d77bc7ff631a881e
Dirk Günther <d.guenther@cenit.de>
2016-01-03 19:04:14 +0100

    Neue Servivemethoden hinzugefügt

--- src/main/resources/scanstation.js
+++ src/main/resources/scanstation.js
@@ -2,8 +2,10 @@ var scanApp = angular.module("scanstation", []);
 
 scanApp.service("scanservice", function($http) {
 
+  var uriRoot="/scan";
+
   this.startscan = function (callback) {
-    $http.get("/scan/batch/start")
+    $http.get(uriRoot + "/start")
       .success(callback)
       .error(callback);
   };
@@ -13,7 +15,18 @@ scanApp.service("scanservice", function($http) {
         "page": page
       }
     };
-    $http.get("/scan/batch/nextpage", config)
+    $http.get(uriRoot + "/nextpage", config)
+      .success(callback)
+      .error(callback);
+  };
+  this.lastpage = function (callback, page, documenttitle) {
+    var config = {
+      "params" : {
+        "page": page,
+        "documenttitle": documenttitle
+      }
+    };
+    $http.get(uriRoot + "/lastpage", config)
       .success(callback)
       .error(callback);
   };
@@ -23,7 +36,18 @@ scanApp.service("scanservice", function($http) {
         "documenttitle": documenttitle
       }
     };
-    $http.get("/scan/batch/stop", config)
+    $http.get(uriRoot + "/stop", config)
+      .success(callback)
+      .error(callback);
+  };
+
+  this.singlepagescan = function (callback, documenttitle) {
+    var config = {
+      "params" : {
+        "documenttitle": documenttitle
+      }
+    };
+    $http.get(uriRoot + "/startsinglepage", config)
       .success(callback)
       .error(callback);
   };
@@ -51,6 +75,24 @@ scanApp.controller("ScanController", function($scope, scanservice) {
     $scope.status="Scannen...";
     $scope.disabled=true;
   };
+  $scope.singlepagescan = function() {
+    page = 1;
+    var documenttitle = $scope.documenttitle;
+    var callback = function(data, status, headers, config) {
+      if (status == 200) {
+        $scope.status="Dokument " + documenttitle + " erfolgreich hinzugefügt";
+      }
+      else {
+        alert("Callback");
+      }
+      $scope.inprogress=false;
+      $scope.disabled=false;
+      $scope.documenttitle="";
+    };
+    scanservice.singlepagescan(callback, documenttitle);
+    $scope.status="Dokument wird verarbeitet...";
+    $scope.disabled=true;
+  };
   $scope.nextpage = function() {
     page += 1;
     var callback = function(data, status, headers, config) {
@@ -67,7 +109,8 @@ scanApp.controller("ScanController", function($scope, scanservice) {
     $scope.status="Scannen von Seite " + page + "...";
     $scope.disabled=true;
   };
-  $scope.stopscan = function() {
+  $scope.lastpage = function() {
+    page += 1;
     var documenttitle = $scope.documenttitle;
     var callback = function(data, status, headers, config) {
       if (status == 200) {
@@ -80,7 +123,7 @@ scanApp.controller("ScanController", function($scope, scanservice) {
       $scope.disabled=false;
       $scope.documenttitle="";
     };
-    scanservice.stopscan(callback, documenttitle);
+    scanservice.lastpage(callback, page, documenttitle);
     $scope.status="Dokument wird verarbeitet...";
     $scope.disabled=true;
 
